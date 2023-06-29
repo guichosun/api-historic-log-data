d37018162a12aa1e31c8284274393763eacf6c80
Luis Alberto <luis.delcampo@globant.com>
2023-06-28 20:29:23 -0600

    I've crated the project structure for historic-data service.

---
+++ src/test/java/com/globant/challenge/openpay/apihistoric/MSHistoricDataAppTests.java
@@ -0,0 +1,13 @@
+package com.globant.challenge.openpay.apihistoric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MSHistoricDataAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
